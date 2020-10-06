9cbaa8caf36d82c56af9e7cf7f790d41e29a7395
Thaanu Perera <thaanup@gmail.com>
2020-10-06 19:46:40 +0530

    Welcome screen UI
    Completion of the UI of welcome screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff3b53e5),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          //mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(80, 60, 80, 0),
+              child: Image.asset('lib/assets/logo.png'),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.fromLTRB(60, 0, 60, 20),
+                    child: Image.asset('lib/assets/welcome_screen_image.png'),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: Text(
+                      'Lorem ipsum dolor\nsit amet',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: 'sf',
+                          fontSize: 22,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400),
+                      //width: double.infinity,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  margin: EdgeInsets.fromLTRB(20, 0, 20, 8),
+                  child: RaisedButton(
+                    child: Container(
+                      width: double.infinity,
+                      child: Text(
+                        'Sign Up',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontFamily: 'sf',
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400),
+                        //width: double.infinity,
+                      ),
+                    ),
+                    elevation: 4,
+                    highlightColor: Color(0xff101010),
+                    color: Colors.black,
+                    padding: EdgeInsets.fromLTRB(15, 12, 15, 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(15.0),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  margin: EdgeInsets.fromLTRB(20, 8, 20, 30),
+                  child: RaisedButton(
+                    child: Container(
+                      width: double.infinity,
+                      child: Text(
+                        'Log In',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontFamily: 'sf',
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500),
+                        //width: double.infinity,
+                      ),
+                    ),
+                    elevation: 4,
+                    highlightColor: Color(0xffcccccc),
+                    color: Colors.white,
+                    padding: EdgeInsets.fromLTRB(15, 12, 15, 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(15.0),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
